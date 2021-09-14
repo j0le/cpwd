96c54d15c906845df03ba477afcf3945f64df304
Ole Hüser <j0le@users.noreply.github.com>
2021-09-14 21:28:06 +0200

    Try to use boost::nowide

--- cpwd/cpwd.cpp
+++ cpwd/cpwd.cpp
@@ -1,11 +1,16 @@
 // cpwd.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+//#include <iostream>
+#include <nowide/iostream.hpp>
+#include <nowide/args.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+	nowide::args a(argc, argv);
+
+    nowide::cout << "Hello World!\n";
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
