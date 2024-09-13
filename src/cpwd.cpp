231ec8bc7fda0cf50f67d6ec5fe3b372c1d37839
Ole Hüser <j0le@users.noreply.github.com>
2024-09-13 17:24:04 +0200

    Don't exit if we don't have a console

--- src/cpwd.cpp
+++ src/cpwd.cpp
@@ -15,26 +15,26 @@ int main(/*int argc, char** argv*/)
 	auto cwd = std::filesystem::current_path(ec);
 	const std::wstring cwd_wstr = cwd.native();
 	if (ec) {
-		nowide::cerr << "error!\n";
+		nowide::cerr << "Error: Cannot get current Path.\n";
 		return 1;
 	}
 
-	const HWND console_window = GetConsoleWindow();
+	const HWND window = GetConsoleWindow();
 	static_assert(static_cast<HWND>(NULL) == nullptr);
 
-	if (console_window == nullptr) {
-		nowide::cerr << "cannot get ConsoleWindow.\n";
-		return 1;
+	if (window == nullptr) {
+		nowide::cerr << "Warning: Cannot get ConsoleWindow.\n";
+		// continue with window == NULL
 	}
 
-	if (!OpenClipboard(console_window)) {
-		nowide::cerr << "cannot open Clipboard.\n";
+	if (!OpenClipboard(window)) {
+		nowide::cerr << "Error: Cannot open Clipboard.\n";
 		return 1;
 	}
 
 	if (!EmptyClipboard()) {
 		(void)CloseClipboard();
-		nowide::cerr << "Clipboard could not be emptied.\n";
+		nowide::cerr << "Error: Clipboard could not be emptied.\n";
 		return 1;
 	}
 
@@ -47,7 +47,7 @@ int main(/*int argc, char** argv*/)
 	static_assert(static_cast<HGLOBAL>(NULL) == nullptr);
 	if (mem_handle == nullptr) {
 		(void)CloseClipboard();
-		nowide::cerr << "Global memory could not be allocated.\n";
+		nowide::cerr << "Error: Global memory could not be allocated.\n";
 		return 1;
 	}
 
@@ -56,7 +56,7 @@ int main(/*int argc, char** argv*/)
 	if (mem_ptr == nullptr) {
 		(void)GlobalFree(mem_handle);
 		(void)CloseClipboard();
-		nowide::cerr << "Global memory could not be accessed.\n";
+		nowide::cerr << "Error: Global memory could not be accessed.\n";
 		return 1;
 	}
 	auto wchar_ptr = cwd_wstr.c_str();
@@ -64,7 +64,7 @@ int main(/*int argc, char** argv*/)
 
 	if (GlobalUnlock(mem_handle)!=0 || GetLastError()!=NO_ERROR) {
 		(void)CloseClipboard();
-		nowide::cerr << "Global memory could not be unlocked.\n";
+		nowide::cerr << "Error: Global memory could not be unlocked.\n";
 		return 1;
 	}
 
@@ -72,18 +72,18 @@ int main(/*int argc, char** argv*/)
 	if (SetClipboardData(CF_UNICODETEXT, mem_handle) == nullptr) {
 		(void)GlobalFree(mem_handle);
 		(void)CloseClipboard();
-		nowide::cerr << "Clipboard content could not be set.\n";
+		nowide::cerr << "Error: Clipboard content could not be set.\n";
 		return 1;
 	}
 
 	if (!CloseClipboard()) {
-		nowide::cerr << "Clipboard could not be closed.\n";
+		nowide::cerr << "Error: Clipboard could not be closed.\n";
 		return 1;
 	}
 
 
-	nowide::cerr << u8"The path to the current working directory/folder was successfully copyied to the system clipboard.\n"
-		"The path to current working directory/folder is:\n" << std::flush;
+	nowide::cerr << u8"Info: The path to the current working directory/folder was successfully copyied to the system clipboard.\n"
+		"Info: The path to current working directory/folder is:\n" << std::flush;
 	nowide::cout << cwd.u8string() << u8"\n" << std::flush;
 	return 0;
 }
